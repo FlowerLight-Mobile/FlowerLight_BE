b401b1d455230cbf031c05128e3bd08f63e8e728
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2023-05-22 13:35:54 +0700

    done update User

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,7 +5,6 @@ const userController = {
     getAllUsers: async(req, res) => {
         try {
             const user = await User.find()
-            if (user) {}
             res.status(200).json(user)
         } catch (err) {
             res.status(500).json(err)
@@ -20,6 +19,41 @@ const userController = {
             } else {
                 res.status(401).json({ message: 'User does not exist !' })
             };
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    },
+    updateInfoUser: async(req, res) => {
+        try {
+            function validateName(name) {
+                const re = /^[a-zA-Z ]*$/;
+                return re.test(name);
+            }
+            const name = req.body.name;
+            if (!validateName(name)) {
+                return res.status(401).json({ message: 'Invalid name' });
+            }
+            const updateUser = await User.findByIdAndUpdate(req.params.id, {
+                name: name,
+                apartment: req.body.apartment,
+                street: req.body.street,
+                provider: req.body.provider,
+                district: req.body.district,
+                city: req.body.city,
+            });
+            if (updateUser) {
+                return res.status(200).json(updateUser);
+            } else {
+                return res.status(401).json({ message: 'User does not exist !' });
+            }
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    },
+    //Reset Password
+    resetPassword: async(req, res) => {
+        try {
+
         } catch (err) {
             return res.status(500).json(err);
         }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,10 +8,12 @@ router.get("/", middlewareController.verifyToken, userController.getAllUsers);
 
 //Get User by ID
 router.get("/user/:id", middlewareController.verifyToken, userController.getUser);
-
-//Update Password
+//Update information User
+router.put("/user/update-info/:id", middlewareController.verifyToken, userController.updateInfoUser);
+//Change Password
 router.put("/user/:id/change-password", middlewareController.verifyToken, userController.changePassword);
+//Reset Pasword 
 
 //DELETE User
-router.delete("/user/:id", middlewareController.verifyTokenisAdmin, userController.deleteUser)
+router.delete("/user/:id", middlewareController.verifyTokenisAdmin, userController.deleteUser);
 module.exports = router

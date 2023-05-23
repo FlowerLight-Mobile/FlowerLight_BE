fa785aaf35bf923dc59dbc780f0a8bc0680697b7
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2023-05-23 13:42:37 +0700

    Debug updateUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -29,25 +29,30 @@ const userController = {
                 const re = /^[a-zA-Z ]*$/;
                 return re.test(name);
             }
-            const name = req.body.name;
+
+            const { name, apartment, street, provider, district, city } = req.body;
+
             if (!validateName(name)) {
                 return res.status(401).json({ message: 'Invalid name' });
             }
+
             const updateUser = await User.findByIdAndUpdate(req.params.id, {
                 name: name,
-                apartment: req.body.apartment,
-                street: req.body.street,
-                provider: req.body.provider,
-                district: req.body.district,
-                city: req.body.city,
+                apartment: apartment,
+                street: street,
+                provider: provider,
+                district: district,
+                city: city,
             });
+
             if (updateUser) {
-                return res.status(200).json(updateUser);
+                const updatedUser = await User.findById(req.params.id);
+                res.status(200).json(updatedUser);
             } else {
-                return res.status(401).json({ message: 'User does not exist !' });
+                res.status(401).json({ message: 'User does not exist!' });
             }
         } catch (err) {
-            return res.status(500).json(err);
+            res.status(500).json(err);
         }
     },
     //Reset Password
